afaa5ea21c4a8634250a800f4eceb38b5f591c1b
DN-98 <deahnisadna@gmail.com>
2022-03-29 10:57:55 +0700

    3292022

---
+++ sesi_12/UnitTest_Mock/Services/IEmployeeService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using UnitTest_Mock.Models;
+
+namespace UnitTest_Mock.Sevices
+{
+    public interface IEmployeeService
+    {
+        Task<string> GetEmployeebyId(int id);
+        Task<EmployeeItem> GetEmployeeDetails(int id);
+    }
+
+}
---
+++ sesi_12/UnitTest_Mock1/Models/ApiDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace UnitTest_Mock.Models
+{
+    public class ApiDbContext : DbContext
+    {
+        public virtual DbSet<EmployeeItem> Employee { get; set; }
+
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
+
+
+
